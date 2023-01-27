4a38fc105751bc96157b776915277103d0a43207
tobexint <tobexint@gmail.com>
2023-01-27 02:24:22 +0000

    Pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,7 +9,7 @@ def pascal_triangle(n):
     if (n <= 0):
         return lst
     lst.append([1])
-    for i in range(n -1):
+    for i in range(n - 1):
         lst.append([1] + [lst[i][j] + lst[i][j + 1]
-                    for j in range(len(lst[i]) - 1)] + [1])
+                          for j in range(len(lst[i]) - 1)] + [1])
     return lst
